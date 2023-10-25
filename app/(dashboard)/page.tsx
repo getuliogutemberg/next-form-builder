cfa046fd238f79e5f7102c4a4746bf1bc36a3d0e
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-25 00:09:17 -0300

    stats Card/ prisma Client

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,103 @@
-
+import { GetFormsStats } from "@/actions/form"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Suspense } from "react"
+import {LuView} from 'react-icons/lu'
+import { FaWpforms } from 'react-icons/fa'
+import { HiCursorClick } from 'react-icons/hi'
+import { TbArrowBounce } from 'react-icons/tb'
 
 export default function Home() {
   return (
-    <div>Hello Vercel</div>
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}><CardStatsWrapper /></Suspense>
+    </div>
   )
+}
+
+async function CardStatsWrapper() {
+  const stats = await GetFormsStats()
+  return (
+    <StatsCards loading={false} data={stats} />
+  )
+}
+
+interface StatsCardProps {
+  data?: Awaited<ReturnType<typeof GetFormsStats>>;
+  loading: boolean;
+}
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-col-4">
+     <StatsCard 
+     title="Total Visits" 
+     icon={<LuView className='text-blue-600' />}
+     helperText="Total number of visits" 
+     value={data?.visits.toLocaleString()|| ''}
+     loading={loading}
+     className="shadow-md shadow-blue-600"
+     />
+     <StatsCard 
+     title="Total Submissions" 
+     icon={<FaWpforms className='text-yellow-600' />}
+     helperText="Total number of submissions" 
+     value={data?.visits.toLocaleString()|| ''}
+     loading={loading}
+     className="shadow-md shadow-yellow-600"
+     />
+     <StatsCard 
+     title="Submission Rate" 
+     icon={<HiCursorClick className='text-green-600' />}
+     helperText="Visits that result in a submission" 
+     value={data?.visits.toLocaleString() + '%'|| ''}
+     loading={loading}
+     className="shadow-md shadow-green-600"
+     />
+     <StatsCard 
+     title="Submission Bounce Rate" 
+     icon={<TbArrowBounce className='text-red-600' />}
+     helperText="Visits that leave without submitting" 
+     value={data?.visits.toLocaleString() + '%'|| ''}
+     loading={loading}
+     className="shadow-md shadow-red-600"
+     />
+    </div>
+  )
+}
+
+function StatsCard({
+  title,
+  icon,
+  helperText,
+  value,
+  loading,
+  className,
+}:{
+  title: string
+  icon: React.ReactNode
+  helperText: string
+  value: string
+  loading: boolean
+  className: string
+}) {
+  return <Card className={className}>
+    <CardHeader className="flex flex-row items-center justify-between pb-2">
+      <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
+      {icon}
+    </CardHeader>
+    <CardContent>
+      <div className="text-2xl font-bold">
+        {
+          loading && <Skeleton>
+            <span className="opacity-0">0</span>
+          </Skeleton>
+        }
+        {!loading && value}
+
+      </div>
+      <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+    </CardContent>
+  </Card>
 }
